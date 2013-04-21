b3eb5e83b9b8f39290e7c76a4589819f60691b13
Steve4448 <hohner13@hotmail.com>
2013-04-21 14:22:03 -0400

    Proper obsidian texture.

--- src/nexustools/vanillaexpanded/block/BlockButtonObsidian.java
+++ src/nexustools/vanillaexpanded/block/BlockButtonObsidian.java
@@ -4,6 +4,6 @@ import net.minecraft.block.BlockButton;
 
 public class BlockButtonObsidian extends BlockButton {
     public BlockButtonObsidian(int id, int texId) {
-        super(id, texId, false);
+        super(id, 37, false);
     }
 }
